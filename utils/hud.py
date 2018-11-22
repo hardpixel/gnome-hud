c6e66dcd4bea20eca5e3fb3b414cec2c50ecd741
Jonian Guveli <jonian@hardpixel.eu>
2018-11-22 13:51:42 +0200

    small fixes

--- utils/hud.py
+++ utils/hud.py
@@ -5,23 +5,16 @@ import gi
 gi.require_version('Keybinder', '3.0')
 
 from gi.repository import Keybinder, GLib
-from dbus.mainloop.glib import DBusGMainLoop
 
 from rofi import RofiMenu
 
 
 def hud(_keystr):
-  try:
-    rofi_menu = RofiMenu()
-    rofi_menu.open()
-
-  except AttributeError:
-    return False
+  rofi_menu = RofiMenu()
+  rofi_menu.open()
 
 
 if __name__ == "__main__":
-  DBusGMainLoop(set_as_default=True)
-
   Keybinder.init()
   Keybinder.bind('<Ctrl><Alt>space', hud)
 
