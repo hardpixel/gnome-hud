780a149e73956db760e79cdbad43eb8b73e05469
Jonian Guveli <jonian@hardpixel.eu>
2018-11-23 23:05:02 +0200

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
   long_description = fh.read()
 
 setuptools.setup(
-  name='gnomehud',
+  name='gnome-hud',
   version='1.0.0',
   author='Jonian Guveli',
   author_email='jonian@hardpixel.eu',
