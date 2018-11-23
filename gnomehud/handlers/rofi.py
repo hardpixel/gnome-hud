c1fdf1d61297ab3af92af0609179839a1248e3ff
Jonian Guveli <jonian@hardpixel.eu>
2018-11-23 16:29:27 +0200

    optimize rofi handler
    use dark theme variant

--- gnomehud/handlers/rofi.py
+++ gnomehud/handlers/rofi.py
@@ -1,103 +1,115 @@
 import gi
-import subprocess
 
 gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
+
+from subprocess import Popen
+from subprocess import PIPE
+
 from gnomehud.utils.menu import DbusMenu
+
+
+def rgba_to_hex(color):
+  red   = int(color.red * 255)
+  green = int(color.green * 255)
+  blue  = int(color.blue * 255)
+
+  return "#{0:02x}{1:02x}{2:02x}".format(red, green, blue)
 
 
 class RofiMenu:
 
   def __init__(self):
-    self.dbus   = DbusMenu()
-    self.prompt = self.parse_prompt(self.dbus.prompt)
-    self.items  = self.parse_items(self.dbus.actions)
-    self.menu   = self.open_menu()
+    self.settings  = Gtk.Settings.get_default()
+    self.context   = Gtk.Window().get_style_context()
+    self.dbus_menu = DbusMenu()
+
+    self.settings.set_property('gtk-application-prefer-dark-theme', True)
 
   @property
 
   def selection(self):
-    selection = self.menu.communicate()[0].decode('utf8').rstrip()
-    self.menu.stdin.close()
+    selection = self.menu_proc.communicate()[0].decode('utf8').rstrip()
+    self.menu_proc.stdin.close()
 
     return selection
 
-  def run(self):
-    self.dbus.activate(self.selection)
+  @property
 
-  def rgba_to_hex(self, color):
-    red   = int(color.red * 255)
-    green = int(color.green * 255)
-    blue  = int(color.blue * 255)
+  def prompt(self):
+    return self.dbus_menu.prompt.strip()
 
-    return "#{0:02x}{1:02x}{2:02x}".format(red, green, blue)
+  @property
 
-  def parse_items(self, menu_keys):
-    string, *menu_items = menu_keys
+  def items(self):
+    string, *menu_items = self.dbus_menu.actions
 
     for menu_item in menu_items:
       string += '\n' + menu_item
 
     return string.encode('utf-8')
 
-  def parse_prompt(self, prompt):
-    return prompt.strip()
+  @property
+
+  def font_name(self):
+    return self.settings.get_property('gtk-font-name')
+
+  @property
 
   def gtk_theme_colors(self):
-    gtk_window       = Gtk.Window()
-    style_context    = gtk_window.get_style_context()
-    gtk_theme_colors = {
-      'bg':           style_context.lookup_color('theme_bg_color')[1],
-      'fg':           style_context.lookup_color('theme_fg_color')[1],
-      'selected_bg':  style_context.lookup_color('theme_selected_bg_color')[1],
-      'selected_fg':  style_context.lookup_color('theme_selected_fg_color')[1],
-      'error_bg':     style_context.lookup_color('error_bg_color')[1],
-      'error_fg':     style_context.lookup_color('error_fg_color')[1],
-      'info_bg':      style_context.lookup_color('info_bg_color')[1],
-      'unfocused_fg': style_context.lookup_color('theme_unfocused_fg_color')[1],
-      'unfocused_bg': style_context.lookup_color('theme_unfocused_bg_color')[1]
+    colors = {
+      'bg':           self.lookup_color('theme_bg_color'),
+      'fg':           self.lookup_color('theme_fg_color'),
+      'selected_bg':  self.lookup_color('theme_selected_bg_color'),
+      'selected_fg':  self.lookup_color('theme_selected_fg_color'),
+      'error_bg':     self.lookup_color('error_bg_color'),
+      'error_fg':     self.lookup_color('error_fg_color'),
+      'info_bg':      self.lookup_color('info_bg_color'),
+      'unfocused_fg': self.lookup_color('theme_unfocused_fg_color'),
+      'unfocused_bg': self.lookup_color('theme_unfocused_bg_color')
     }
 
-    for name, color in gtk_theme_colors.items():
-      gtk_theme_colors[name] = self.rgba_to_hex(color)
+    for name, color in colors.items():
+      colors[name] = rgba_to_hex(color)
 
-    return gtk_theme_colors
+    return colors
+
+
+  @property
 
   def theme_colors(self):
-    gtk_colors   = self.gtk_theme_colors()
-    theme_colors = {
+    colors = {
       'window': [
-        gtk_colors['bg'],
-        gtk_colors['unfocused_fg'],
-        gtk_colors['unfocused_fg']
+        self.gtk_theme_colors['bg'],
+        self.gtk_theme_colors['unfocused_fg'],
+        self.gtk_theme_colors['unfocused_fg']
       ],
       'normal': [
-        gtk_colors['bg'],
-        gtk_colors['fg'],
-        gtk_colors['bg'],
-        gtk_colors['selected_bg'],
-        gtk_colors['selected_fg']
+        self.gtk_theme_colors['bg'],
+        self.gtk_theme_colors['fg'],
+        self.gtk_theme_colors['bg'],
+        self.gtk_theme_colors['selected_bg'],
+        self.gtk_theme_colors['selected_fg']
       ],
       'urgent': [
-        gtk_colors['bg'],
-        gtk_colors['fg'],
-        gtk_colors['bg'],
-        gtk_colors['error_bg'],
-        gtk_colors['error_fg']
+        self.gtk_theme_colors['bg'],
+        self.gtk_theme_colors['fg'],
+        self.gtk_theme_colors['bg'],
+        self.gtk_theme_colors['error_bg'],
+        self.gtk_theme_colors['error_fg']
       ]
     }
 
-    for name, colors in theme_colors.items():
-      theme_colors[name] = ', '.join(colors)
+    for name, color in colors.items():
+      colors[name] = ', '.join(color)
 
-    return theme_colors
+    return colors
+
+  def lookup_color(self, key):
+    return self.context.lookup_color(key)[1]
 
   def open_menu(self):
-    settings     = Gtk.Settings.get_default()
-    font_name    = settings.get_property('gtk-font-name')
-    theme_colors = self.theme_colors()
-
     settings = [
       'rofi',
       '-i',
@@ -106,16 +118,19 @@ class RofiMenu:
       '-color-enabled',
       '-lines', '6',
       '-location', '2',
-      '-width', '100',
+      '-yoffset', '32',
+      '-width', '800',
       '-bw', '0',
       '-p', self.prompt,
-      '-font', font_name,
-      '-color-window', theme_colors['window'],
-      '-color-normal', theme_colors['normal'],
-      '-color-urgent', theme_colors['urgent']
+      '-font', self.font_name,
+      '-color-window', self.theme_colors['window'],
+      '-color-normal', self.theme_colors['normal'],
+      '-color-urgent', self.theme_colors['urgent']
     ]
 
-    menu = subprocess.Popen(settings, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    menu.stdin.write(self.items)
+    self.menu_proc = Popen(settings, stdout=PIPE, stdin=PIPE)
+    self.menu_proc.stdin.write(self.items)
 
-    return menu
+  def run(self):
+    self.open_menu()
+    self.dbus_menu.activate(self.selection)
