15acd12565b4b72f98e374841e40b558d7d13000
Jonian Guveli <jonian@hardpixel.eu>
2018-11-23 21:09:43 +0200

    do not run service if already running

--- gnomehud/utils/service.py
+++ gnomehud/utils/service.py
@@ -3,30 +3,30 @@ import dbus.service
 
 from gi.repository import GLib
 
-service_name = 'com.canonical.AppMenu.Registrar'
-service_path = '/com/canonical/AppMenu/Registrar'
+BUS_NAME = 'com.canonical.AppMenu.Registrar'
+BUS_PATH = '/com/canonical/AppMenu/Registrar'
 
 
 class AppMenuService(dbus.service.Object):
   def __init__(self):
     self.window_dict = dict()
 
-    bus_name = dbus.service.BusName(service_name, bus=dbus.SessionBus())
-    dbus.service.Object.__init__(self, bus_name, service_path)
+    bus_name = dbus.service.BusName(BUS_NAME, bus=dbus.SessionBus())
+    dbus.service.Object.__init__(self, bus_name, BUS_PATH)
 
-  @dbus.service.method(service_name, in_signature='uo', sender_keyword='sender')
+  @dbus.service.method(BUS_NAME, in_signature='uo', sender_keyword='sender')
 
   def RegisterWindow(self, windowId, menuObjectPath, sender):
     self.window_dict[windowId] = (sender, menuObjectPath)
 
-  @dbus.service.method(service_name, in_signature='u', out_signature='so')
+  @dbus.service.method(BUS_NAME, in_signature='u', out_signature='so')
 
   def GetMenuForWindow(self, windowId):
     if windowId in self.window_dict:
       sender, menuObjectPath = self.window_dict[windowId]
       return [dbus.String(sender), dbus.ObjectPath(menuObjectPath)]
 
-  @dbus.service.method(service_name)
+  @dbus.service.method(BUS_NAME)
 
   def Q(self):
     GLib.MainLoop().quit()
