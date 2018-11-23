047b4d599faede0cb609c6c6bba814932543fa49
Jonian Guveli <jonian@hardpixel.eu>
2018-11-23 22:39:53 +0200

    remove systemd service

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setuptools.setup(
   url='https://github.com/hardpixel/gnome-hud',
   packages=setuptools.find_packages(),
   data_files=[
-    ('lib/systemd/system', ['gnomehud.service']),
     ('share/applications', ['gnomehud.desktop', 'gnomehud-rofi.desktop'])
   ],
   install_requires=[
