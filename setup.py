6992db7fe2937e8d9278ca889bf43a0bb3d63ad3
Jonian Guveli <jonian@hardpixel.eu>
2018-11-24 23:31:54 +0200

    bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
   name='gnome-hud',
-  version='1.1.1',
+  version='1.2.0',
   author='Jonian Guveli',
   author_email='jonian@hardpixel.eu',
   description='Unity like HUD menu for the GNOME Desktop Environment',
