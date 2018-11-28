0aec712d04487df00fa38bed47d46e32e32440e9
Jonian Guveli <jonian@hardpixel.eu>
2018-11-28 15:32:30 +0200

    refactor dbus menu explorer

--- gnomehud/utils/menu.py
+++ gnomehud/utils/menu.py
@@ -3,177 +3,184 @@ import dbus
 
 gi.require_version('Bamf', '3')
 
-from gi.repository import Gdk
 from gi.repository import Gio
 from gi.repository import Bamf
 
-SEPARATOR = u'\u0020\u0020\u00BB\u0020\u0020'
+from gnomehud.utils.fuzzy import match_replace
+
+
+def format_label(parts):
+  separator = u'\u0020\u0020\u00BB\u0020\u0020'
+  return separator.join(parts)
+
+
+def normalize_label(text):
+  text = match_replace('\_|\.\.\.', '', text)
+  text = match_replace('\s+', ' ', text)
+
+  return text.strip()
+
+
+class DbusGtkMenuItem(object):
+
+  def __init__(self, item, path=[]):
+    self.path   = path
+    self.action = str(item.get('action', ''))
+    self.accel  = str(item.get('accel', ''))
+    self.label  = normalize_label(item.get('label', ''))
+    self.text   = format_label(self.path + [self.label])
+
+
+class DbusGtkMenu(object):
+
+  def __init__(self, session, window):
+    self.results      = {}
+    self.actions      = {}
+    self.session      = session
+    self.bus_name     = window.get_utf8_prop('_GTK_UNIQUE_BUS_NAME')
+    self.app_path     = window.get_utf8_prop('_GTK_APPLICATION_OBJECT_PATH')
+    self.win_path     = window.get_utf8_prop('_GTK_WINDOW_OBJECT_PATH')
+    self.menubar_path = window.get_utf8_prop('_GTK_MENUBAR_OBJECT_PATH')
+    self.appmenu_path = window.get_utf8_prop('_GTK_APP_MENU_OBJECT_PATH')
+
+  def activate(self, selection):
+    action = self.actions.get(selection, '')
+
+    if 'app.' in action:
+      self.send_action(action, 'app.', self.app_path)
+    elif 'win.' in action:
+      self.send_action(action, 'win.', self.win_path)
+    elif 'unity.' in action:
+      self.send_action(action, 'unity.', self.menubar_path)
+
+  def send_action(self, name, prefix, path):
+    object    = self.session.get_object(self.bus_name, path)
+    interface = dbus.Interface(object, dbus_interface='org.gtk.Actions')
+
+    interface.Activate(name.replace(prefix, ''), [], dict())
+
+  def get_results(self):
+    paths = [self.appmenu_path, self.menubar_path]
+
+    for path in filter(None, paths):
+      object    = self.session.get_object(self.bus_name, path)
+      interface = dbus.Interface(object, dbus_interface='org.gtk.Menus')
+      results   = interface.Start([x for x in range(1024)])
+
+      for menu in results:
+        self.results[(menu[0], menu[1])] = menu[2]
+
+    self.collect_entries([0, 0])
+
+  def collect_entries(self, menu, labels=[]):
+    for menu in self.results.get((menu[0], menu[1]), []):
+      if 'label' in menu:
+        menu_item = DbusGtkMenuItem(menu, labels)
+        menu_path = labels + [menu_item.label]
+
+        if ':submenu' in menu:
+          self.collect_entries(menu[':submenu'], menu_path)
+        elif 'action' in menu:
+          self.actions[menu_item.text] = menu_item.action
+
+      elif ':section' in menu:
+        self.collect_entries(menu[':section'], labels)
+
+
+class DbusAppMenuItem(object):
+
+  def __init__(self, item, path=[]):
+    self.path   = path
+    self.action = int(item[0])
+    self.accel  = None
+    self.label  = normalize_label(item[1].get('label', ''))
+    self.text   = format_label(self.path + [self.label])
+
+
+class DbusAppMenu(object):
+
+  def __init__(self, session, window):
+    self.actions   = {}
+    self.session   = session
+    self.xid       = window.get_xid()
+    self.interface = self.get_interface()
+
+  def activate(self, selection):
+    action = self.actions[selection]
+    self.interface.Event(action, 'clicked', 0, 0)
+
+  def get_interface(self):
+    bus_name = 'com.canonical.AppMenu.Registrar'
+    bus_path = '/com/canonical/AppMenu/Registrar'
+
+    try:
+      object     = self.session.get_object(bus_name, bus_path)
+      interface  = dbus.Interface(object, bus_name)
+      name, path = interface.GetMenuForWindow(self.xid)
+      object     = self.session.get_object(name, path)
+      interface  = dbus.Interface(object, 'com.canonical.dbusmenu')
+
+      return interface
+    except dbus.exceptions.DBusException:
+      return None
+
+  def get_results(self):
+    if self.interface:
+      results = self.interface.GetLayout(0, -1, ['label'])
+      self.collect_entries(results[1], [])
+
+  def collect_entries(self, item=None, labels=[]):
+    menu_item = DbusAppMenuItem(item, labels)
+    menu_path = labels
+
+    if menu_item.label != 'Root':
+      menu_path = labels + [menu_item.label]
+
+    if len(item[2]):
+      for child in item[2]:
+        self.collect_entries(child, menu_path)
+
+    elif bool(menu_item.label):
+      self.actions[menu_item.text] = menu_item.action
 
 
 class DbusMenu:
 
   def __init__(self):
-    self.session      = dbus.SessionBus()
-    self.screen       = Gdk.Screen.get_default()
-    self.gdk_window   = self.screen.get_active_window()
-    self.matcher      = Bamf.Matcher.get_default()
-    self.window       = self.matcher.get_active_window()
-    self.xid          = self.window.get_xid()
-    self.bus_name     = self.window.get_utf8_prop('_GTK_UNIQUE_BUS_NAME')
-    self.app_path     = self.window.get_utf8_prop('_GTK_APPLICATION_OBJECT_PATH')
-    self.win_path     = self.window.get_utf8_prop('_GTK_WINDOW_OBJECT_PATH')
-    self.menubar_path = self.window.get_utf8_prop('_GTK_MENUBAR_OBJECT_PATH')
-    self.appmenu_path = self.window.get_utf8_prop('_GTK_APP_MENU_OBJECT_PATH')
-    self.menu_items   = dict()
-    self.menu_actions = dict()
-
-    self.explore_dbusmenu()
-    self.explore_gtkmenu()
-    self.handle_empty()
+    self.session = dbus.SessionBus()
+    self.matcher = Bamf.Matcher.get_default()
+    self.window  = self.matcher.get_active_window()
+    self.appmenu = DbusAppMenu(self.session, self.window)
+    self.gtkmenu = DbusGtkMenu(self.session, self.window)
 
   @property
 
   def prompt(self):
-    application  = self.matcher.get_active_application()
-    desktop_file = application.get_desktop_file()
-    deskapp_info = Gio.DesktopAppInfo.new_from_filename(desktop_file)
+    app  = self.matcher.get_active_application()
+    file = app.get_desktop_file()
+    info = Gio.DesktopAppInfo.new_from_filename(file)
 
-    return deskapp_info.get_string('Name')
+    return info.get_string('Name')
 
   @property
 
   def actions(self):
-    return self.menu_actions.keys()
+    self.appmenu.get_results()
+    self.gtkmenu.get_results()
+
+    actions = { **self.gtkmenu.actions, **self.appmenu.actions }
+    self.handle_empty(actions)
+
+    return actions.keys()
 
   def activate(self, selection):
-    if selection:
-      self.activate_item(selection)
+    if selection in self.gtkmenu.actions:
+      self.gtkmenu.activate(selection)
 
-  def activate_item(self, selection):
-    if self.dbusmenu:
-      self.activate_dbusmenu_item(selection)
-    else:
-      self.activate_gtkmenu_item(selection)
+    elif selection in self.appmenu.actions:
+      self.appmenu.activate(selection)
 
-  def activate_dbusmenu_item(self, selection):
-    action = self.menu_actions[selection]
-    self.dbusmenu.Event(action, 'clicked', 0, 0)
-
-  def activate_gtkmenu_item(self, selection):
-    action = self.menu_actions.get(selection, 'sys.quit')
-
-    if 'sys.' in action:
-      self.close_window()
-
-    elif 'app.' in action:
-      self.activate_gtkmenu_action(action, 'app.', self.app_path)
-
-    elif 'win.' in action:
-      self.activate_gtkmenu_action(action, 'win.', self.win_path)
-
-    elif 'unity.' in action:
-      self.activate_gtkmenu_action(action, 'unity.', self.menubar_path)
-
-  def activate_gtkmenu_action(self, action, prefix, path):
-    object = self.session.get_object(self.bus_name, path)
-    iface  = dbus.Interface(object, dbus_interface='org.gtk.Actions')
-
-    iface.Activate(action.replace(prefix, ''), [], dict())
-
-  def close_window(self):
-    self.gdk_window.destroy()
-
-  def explore_dbusmenu(self):
-    name = 'com.canonical.AppMenu.Registrar'
-    path = '/com/canonical/AppMenu/Registrar'
-
-    try:
-      object     = self.session.get_object(name, path)
-      interface  = dbus.Interface(object, name)
-      name, path = interface.GetMenuForWindow(self.xid)
-      pobject    = self.session.get_object(name, path)
-      interface  = dbus.Interface(pobject, 'com.canonical.dbusmenu')
-      results    = interface.GetLayout(0, -1, ['label'])
-
-      self.explore_dbusmenu_items(results[1], [])
-      self.dbusmenu = interface
-    except dbus.exceptions.DBusException:
-      self.dbusmenu = None
-
-  def explore_dbusmenu_items(self, item=None, labels=None):
-    item_id       = item[0]
-    item_props    = item[1]
-    item_children = item[2]
-
-    if 'label' in item_props:
-      new_labels = labels + [item_props['label']]
-    else:
-      new_labels = labels
-
-    if len(item_children) == 0:
-      if 'label' in item_props:
-        form_label = self.format_labels(new_labels)
-        self.menu_actions[form_label] = item_id
-    else:
-      for child in item_children:
-        self.explore_dbusmenu_items(child, new_labels)
-
-  def explore_gtkmenu(self):
-    paths = [self.appmenu_path, self.menubar_path]
-
-    for path in filter(None, paths):
-      pobject   = self.session.get_object(self.bus_name, path)
-      interface = dbus.Interface(pobject, dbus_interface='org.gtk.Menus')
-      results   = interface.Start([x for x in range(1024)])
-
-      for result in results:
-        self.menu_items[(result[0], result[1])] = result[2]
-
-    self.explore_gtkmenu_items()
-
-  def explore_gtkmenu_items(self, menu_id=None, labels=None):
-    menu_id = menu_id or (0, 0)
-    labels  = labels or list()
-
-    for menu in self.menu_items.get(menu_id, list()):
-      if 'label' in menu:
-        label      = menu['label']
-        new_labels = labels + [label]
-        form_label = self.format_labels(new_labels)
-
-        if 'action' in menu:
-          menu_action = menu['action']
-
-          if ':section' not in menu and ':submenu' not in menu:
-            self.menu_actions[form_label] = menu_action
-
-      if ':section' in menu:
-        self.explore_gtkmenu_subitems(menu[':section'], labels)
-
-      if ':submenu' in menu:
-        self.explore_gtkmenu_subitems(menu[':submenu'], new_labels)
-
-  def explore_gtkmenu_subitems(self, items, labels):
-    menu_id = (items[0], items[1])
-    self.explore_gtkmenu_items(menu_id, labels)
-
-  def format_labels(self, labels):
-    head, *tail = labels
-    result      = head
-
-    for label in tail:
-      result = result + SEPARATOR + label
-
-    result = result.replace('Root%s' % SEPARATOR, '')
-    result = result.replace('_', '')
-    result = result.replace('...', ' ')
-    result = result.replace('\s+', ' ')
-
-    return result.strip()
-
-  def handle_empty(self):
-    if not len(self.menu_actions):
-      self.menu_actions = { 'Quit': 'sys.quit' }
-
+  def handle_empty(self, actions):
+    if not len(actions):
       alert = 'No menu items available! Showing only "Quit" entry.'
       print('gnomeHUD: WARNING: (%s) %s' % (self.prompt, alert))
