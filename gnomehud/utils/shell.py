ed47e6857842fd3c6498f1e4c6b570aeabad2364
Jonian Guveli <jonian@hardpixel.eu>
2018-11-30 01:33:18 +0200

    add shell window class
    provides active window and app name

--- gnomehud/utils/shell.py
+++ gnomehud/utils/shell.py
@@ -7,6 +7,10 @@ def match_findall(pattern, text):
   return regex.findall(text)
 
 
+def normalize_string(string):
+  return str(string or '').replace('"', '')
+
+
 class DbusShell(object):
 
   def __init__(self):
@@ -28,22 +32,37 @@ class DbusShell(object):
   def eval_script(self, script):
     return self.interface.Eval(script)
 
+  def eval_object_function(self, object, prop):
+    result = self.eval_script('%s.%s()' % (object, prop))
+    return result[1] if bool(result[0]) else None
+
   def get_focus_window_prop(self, prop):
     window = 'global.display.focus_window'
-    result = self.eval_script('%s.%s()' % (window, prop))
+    return self.eval_object_function(window, prop)
 
-    return result[1] if bool(result[0]) else None
+  def get_focus_app_prop(self, prop):
+    app = 'Shell.WindowTracker.get_default().focus_app'
+    return self.eval_object_function(app, prop)
+
+
+class ShellWindow(object):
+
+  def __init__(self):
+    self.matcher = DbusShell()
 
   def get_utf8_prop(self, property):
     method = 'get%s' % property.lower()
-    result = self.get_focus_window_prop(method)
-    result = str(result or '').replace('"', '')
+    result = self.matcher.get_focus_window_prop(method)
+    result = normalize_string(result)
 
     return result if result != 'null' else None
 
   def get_xid(self):
-    result = self.get_focus_window_prop('get_description')
-    result = str(result or '').replace('"', '')
-    result = match_findall('0x[0-9a-f]+', result)
+    result = self.matcher.get_focus_window_prop('get_description')
+    result = match_findall('0x[0-9a-f]+', normalize_string(result))
 
     return int(result[0], 16) if result else None
+
+  def get_appname(self):
+    result = self.matcher.get_focus_app_prop('get_name')
+    return normalize_string(result)
